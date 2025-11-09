6b941874a60c968f2f472d54f2e94f7dc8fc5544
CamiloSerranoT <camilo.a.serrano.t@gmail.com>
2025-11-09 11:55:10 -0500

    Agregar funciones

--- 15-funciones.js
+++ 15-funciones.js
@@ -21,3 +21,54 @@ const finalPrice = calcularDiscountPrince(originalPrice,discountPer);
 console.log(`Original Price: $ ${originalPrice}`);
 console.log(`Discount: ${discountPer}`);
 console.log(`Price with discount: $ ${finalPrice}`);
+
+
+//Posibilidades con funciones
+// 1. Callback - Pasar argumentos
+
+function a () {}
+function b (a) {}
+b(a)
+
+// Retorno de funciones
+
+function a () {
+    function b () {}
+    return b
+}
+
+b(a)
+
+// Asignar funciones a variables --> Expresion de funcion
+
+const a3 = function () {}
+
+// Tener propiedades y metodos
+
+function a () {}
+const obj = {}
+a.call(obj)
+
+// Anidar funciones - Nested functions
+
+function a () {
+    function b () {
+        function c () {
+
+        }
+        c()
+    }
+    b()
+}
+a()
+
+// ¿Es posible almacenar funciones en objetos?
+
+const rocket = {
+    name: 'Falcon 9',
+    launchMessage: function launchMessage () {
+        console.log('Lanzamiento')
+    }
+}
+
+rocket.launchMessage()
